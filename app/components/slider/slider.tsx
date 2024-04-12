b49d9ca0be841920c073471d8c5bddc23fa1d5f2
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-04-12 17:50:59 +0200

    slider position ok

--- app/components/slider/slider.tsx
+++ app/components/slider/slider.tsx
@@ -3,20 +3,30 @@ import React, { useState } from 'react';
 
 export default function Testt() {
 
-    const [progress, setProgress] = useState(30);
+    const [progress, setProgress] = useState(70);
 
     return (
-        <div className='h-full flex'>
-            <div className='bottom-0' style={{ bottom: `20%` }}>
-                { /* eslint-disable-next-line */}
-                <img className="object-none" src='./draggable.svg' draggable={false}></img>
+        <div className='h-full flex flex-row relative'>
+            <div className='relative w-24 min-w-24 h-full' >
+                <div className="absolute object-none bottom-0" style={{ height: `calc(${progress}% + 20px)` }}>
+                    <img className="object-none top-0" src='./draggable.svg' draggable={false}></img>
+                </div>
             </div>
             <div className="relative w-7 min-w-7 h-full bg-slate-300 rounded">
                 <div className="absolute bottom-0 left-0 w-7 h-1/2 bg-sky-950 rounded" style={{ height: `${progress}%` }}></div>
             </div>
-            <div>
-                { /* eslint-disable-next-line */}
-                <img className="transform rotate-180 object-none" src='./draggable.svg' draggable={false}></img>
+            <div className='relative w-24 min-w-24 h-full'>
+                <div className="absolute object-none  bottom-0" style={{ height: `calc(75% + 20px)` }}>
+                    <img className="object-none top-0 transform rotate-180" src='./draggable.svg' draggable={false}></img>
+                </div>
+
+                <div className="absolute object-none  bottom-0" style={{ height: `calc(50% + 20px)` }}>
+                    <img className="object-none top-0 transform rotate-180" src='./draggable.svg' draggable={false}></img>
+                </div>
+
+                <div className="absolute object-none  bottom-0" style={{ height: `calc(25% + 20px)` }}>
+                    <img className="object-none top-0 transform rotate-180" src='./draggable.svg' draggable={false}></img>
+                </div>
             </div>
         </div>
     );
